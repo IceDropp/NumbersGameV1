8c2f23135df45bd6cff1f9e3a957416c9df63af7
Morgan <144250478+IceDropp@users.noreply.github.com>
2023-10-03 16:38:06 +0200

    g

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -2,32 +2,56 @@
 {
     internal class Program
     {
+        static void Main()
+        {
+            int randomNumber = GenerateRandomNumber();
+            int guessedNumber = GetGuesedNumber();
+            NumberCheck(guessedNumber, randomNumber);
+            Console.ReadLine();
+        }
         static int GetGuesedNumber()
         {
             Console.WriteLine("Välkommen jag tänker på ett nummer kan du gissa vilket? Du får fem försök");
 
-            int guessedNumber;
-            if (!int.TryParse(Console.ReadLine(), out guessedNumber))
+            int guessedNumber = 1; /*
+            if (!int.TryParse(Console.ReadLine(), out guessedNumber)) //skräp
             {
                 Console.WriteLine("Felaktig inmatning. Försök igen.");
-                guessedNumber = GetGuesedNumber(); // Recursively prompt for input.
             }
-            // Console.WriteLine(guessedNumber);
-            return guessedNumber;
+            // Console.WriteLine(guessedNumber);*/
+            return guessedNumber; 
         }
         static int GenerateRandomNumber()
         {
             Random random = new Random();
-            int randomNumber = random.Next(1, 20);
-            Console.WriteLine(randomNumber);
+            int randomNumber = random.Next(1, 21);
+      //      Console.WriteLine(randomNumber);  // skriver ut random nummret
             return randomNumber;
         }
 
         static void NumberCheck(int guessedNumber, int randomNumber)
         {
             int countdown = 5;
-            for (int i = 1; i < countdown; i++)
+            for (int i = 1; i <= countdown; i++)
             {
+                int newGuess;
+
+                while (true)
+                {
+                    Console.Write("Kan du gissa?: ");
+                    string input = Console.ReadLine();
+
+                    if (int.TryParse(input, out newGuess) && newGuess != 0)
+                    {
+                        break; // undersöker om detta är en valid integer 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Felaktig inmatning. Försök igen.");
+                    }
+                }
+
+                guessedNumber = newGuess; // Uppdaterar det gissade nummret
 
                 if (guessedNumber == randomNumber)
                 {
@@ -42,35 +66,17 @@
                 {
                     Console.WriteLine("Din gissning var för hög");
                 }
-                Console.Write("Gissa igen: ");
-                //        guessedNumber = int.Parse(Console.ReadLine());
-                if (int.TryParse(Console.ReadLine(), out guessedNumber) && Math.Abs(guessedNumber - randomNumber) <= 1)
-                {
-                    i = -1;
-                    int g = 0;
-                    g++;
-                    Console.WriteLine($"g"+g);
 
+                if (Math.Abs(guessedNumber - randomNumber) <= 1)
+                {
                     Console.WriteLine("Du är nära.");
                 }
- /*               if (Math.Abs(guessedNumber - randomNumber) <= 1)
-                    {
-                        Console.WriteLine("Du är nära.");
-                    }
- */             
-                if ((guessedNumber != randomNumber) && (i == 4))
+
+                if ((guessedNumber != randomNumber) && (i == 5))
                 {
                     Console.WriteLine("Tyvärr inga fler gissningar kvar. Det rätta svaret var: " + randomNumber);
                 }
             }
         }
-            static void Main()
-            {
-                int randomNumber = GenerateRandomNumber();
-                int guessedNumber = GetGuesedNumber();
-                NumberCheck(guessedNumber, randomNumber);
-
-                Console.ReadLine();
-            }
     }
 }
